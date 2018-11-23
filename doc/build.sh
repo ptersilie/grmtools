8afaf6654093997dc3aa7a3a19213f5fdafdb296
Lukas Diekmann <lukas.diekmann@gmail.com>
2018-11-23 14:54:39 +0000

    Checkout the correct repo; Also build api docs

--- doc/build.sh
+++ doc/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# build book for each tag
+# build book/apidoc for each tag
 for tag in $(git tag)
 do
+    git checkout $tag
     mdbook build -d book/$tag
+    cargo doc --target-dir book/api/$tag
 done
 
-# build book for master
+# build book/apidoc for master
 mdbook build -d book/master
+cargo doc --target-dir book/api/master
